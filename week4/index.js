5607201d5ba51d6e49fedc4560e63d5c2d458966
adela <adela.535190006@stu.untar.ac.id>
2020-09-28 16:57:18 +0700

    added new files

---
+++ week4/index.js
@@ -0,0 +1,35 @@
+var items = [];
+
+var info = document.getElementById("info")
+var shownItems = document.getElementById("shownItems")
+
+function clearInfo() {
+    info.innerHTML = "";
+    info.className = "";
+}
+
+function addItem(){
+    clearInfo();
+
+    var item = document.getElementById("item").value;
+
+    if(items.length >= 5) {
+        info.innerHTML = "You have reach maximus possible items.";
+        info.className = "warning";
+    } 
+    else if (item.length == 0){
+        info.innerHTML = "Item cannot be empty.";
+        info.className = "warning";
+    }
+    else {
+        items.push(item);
+        info.innerHTML = "Success.";
+        info.className = "success";
+    }
+}
+
+function showItems() {
+    for(item of items) {
+        shownItems.innerHTML = shownItems.innerHTML + item + "<br>";
+    }
+}
