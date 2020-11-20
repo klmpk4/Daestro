9d864cdca50119721000c0430e451e7e24044fcc
carolineoline <70556000+carolineoline@users.noreply.github.com>
2020-11-20 14:52:52 +0700

    edit new files

---
+++ product.js
@@ -0,0 +1,7 @@
+const state = {
+    product: [
+        { id: '1', name: 'Waist Bag', price: '179.999', image: 'Chain-detail Waist Bag - Light Brown', color: 'Brown', category: 'Bag', },
+        { id: '2', name: 'Cotton Bag', price: '49.999', image: 'Cotton Canvas Bag - Natural White', color: 'White', category: 'Bag', },
+        { id: '3', name: 'C', price: '49999', image: '', color: '', category: 'Bag', },
+    ]
+}
