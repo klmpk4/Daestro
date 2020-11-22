c0ca3545aa13b9cca1704a6ae97af70f386e6b35
carolineoline <70556000+carolineoline@users.noreply.github.com>
2020-11-22 21:41:34 +0700

    modified files

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express'),
-        app = express(),
-        bodyParser = require('body-parser');
+    app = express(),
+    bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.port || 8000;
 
 //routes
-const indexRoutes = require ('./routes/index');
+const indexRoutes = require('./routes/index');
 app.use('/', indexRoutes);
+app.use('/public', express.static('public'));
 
 
 app.listen(8000);
