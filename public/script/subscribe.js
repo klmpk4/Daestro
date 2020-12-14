f5db9d72eebcbed70ac2d202fbc395dadeda3ad4
FernandoLie <70552994+FernandoLie@users.noreply.github.com>
2020-12-14 23:31:55 +0700

    Update subscribe.js

--- public/script/subscribe.js
+++ public/script/subscribe.js
@@ -6,11 +6,6 @@ function ClickSubscribe() {
       response.css("color", "red");
       response.text("Silahkan masukkan alamat email");
     }
-    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(input) )
-    {
-      response.css("color", "red");
-      response.text("Silahkan masukkan alamat email dengan benar");
-    }
     else
     {
       response.css("color" , "green");
